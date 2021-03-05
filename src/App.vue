2b6e799e90d5ea7b99e1b9e34a50be115aab77a6
sony714 <1059093241@qq.com>
2021-03-05 22:41:52 +0800

    引入全局组件<nav/>

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
     App
     <hr />
     <router-view />
-    <router-link to="/money">记账|</router-link>
-    <router-link to="/labels">标签|</router-link>
-    <router-link to="/statistics">统计|</router-link>
   </div>
 </template>
 
